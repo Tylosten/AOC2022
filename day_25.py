28db16c2d2e50981eb28090021fc565b94bfe2bf
Magali Vangkeosay <36881448+Tylosten@users.noreply.github.com>
2023-03-09 22:04:48 +0100

    Day 25

--- day_25.py
+++ day_25.py
@@ -12,11 +12,27 @@ class Puzzle25(Puzzle):
             res += val * (5**i)
         return res
 
-    # def int_to_snafu(self, nb):
+    def int_to_snafu(self, nb):
+        res = []
+        to_process = nb
+        while to_process > 0:
+            res.append(to_process % 5)
+            to_process = to_process // 5
+        # pylint: disable=consider-using-enumerate
+        for i in range(len(res)):
+            if res[i] > 2:
+                res[i] -= 5
+                res[i] = "-" if res[i] == -1 else "=" if res[i] == -2 else str(res[i])
+                if i == len(res) - 1:
+                    res.append(1)
+                else:
+                    res[i + 1] += 1
+
+        return "".join(str(r) for r in reversed(res))
 
     def run_part1(self):
-        sum = sum(self.snafu_to_int(snafu) for snafu in self.input)
-        return sum
+        sum_int = sum(self.snafu_to_int(snafu) for snafu in self.input)
+        return self.int_to_snafu(sum_int)
 
 
 if __name__ == "__main__":
